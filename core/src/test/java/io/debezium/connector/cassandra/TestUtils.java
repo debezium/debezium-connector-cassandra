cf3737613a56ea44ac410f60d2d1ad59e94fe939
Stefan Miklosovic <smiklosovic@apache.org>
2022-05-18 12:49:05 +0200

    DBZ-4401 apply changes to Cassandra 3 implementation

--- core/src/test/java/io/debezium/connector/cassandra/TestUtils.java
+++ core/src/test/java/io/debezium/connector/cassandra/TestUtils.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
-import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 
 import com.datastax.oss.driver.api.core.CqlSession;
@@ -136,15 +135,6 @@ public class TestUtils {
      */
     public static String keyspaceTable(String tableName) {
         return TEST_KEYSPACE_NAME + "." + tableName;
-    }
-
-    /**
-     * Generate a commit log file with current timestamp in memory
-     */
-    public static File generateCommitLogFile() {
-        File cdcDir = new File(DatabaseDescriptor.getCDCLogLocation());
-        long ts = System.currentTimeMillis();
-        return Paths.get(cdcDir.getAbsolutePath(), "CommitLog-6-" + ts + ".log").toFile();
     }
 
     /**
